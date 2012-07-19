525f346c24b9671cbde3fa6a7994facb0a46262e
xa conrad <xa.conrad@yahoo.com>
2012-07-19 14:55:46 -0700

    no message

--- generate_map.py
+++ generate_map.py
@@ -3,6 +3,7 @@
 import sys
 import random
 import json
+from adventure import LEFT, RIGHT, FORWARDS, BACKWARDS
 
 def generate(mapname):
     # TODO pick a random number of rooms to generate
@@ -14,38 +15,71 @@ def generate(mapname):
 
     for room_num in range(room_count):
         # TODO create a room and save it in a dictionary
-        room_name = ""
+        room_name = "room%d" % (room_num + 1)
         room = dict()
+        map_dict[room_name] = room
+        
 
         # TODO create random monsters
-        monster_count = 0
+        monster_count = random.randint(0,2)
         for monster_num in range(monster_count):
             create_monster(monster_num, room)
 
         # TODO create random items
 
-        # TODO connect room to another random room in a random direction
+        if room_num > 0:
+            while True:
+                other_room_num = random.randint(1, room_num)
+                other_room_name = "room%d" % other_room_num
+                direction = random.choice((LEFT, RIGHT, FORWARDS, BACKWARDS))
+                if check_connection(connections, room_name, direction, other_room_name):
+                    connection = { room_name : { direction : other_room_name } }
+                    connections.append(connection)
+                    break
 
-        pass
+    create_wizard(map_dict, room_count)
 
-    create_wizard(map_dict)
-
-    # TODO save map to file 'mapname'
     mapfile = open(mapname, 'w+')
     json.dump(map_dict, mapfile, indent=4)
     mapfile.close()
 
-def create_monster(monster_num, room):
-    pass
-    # TODO - pick a random health
-    # TODO - create the monster with the health
-    # TODO - add the monster to the current room
+def opposite(direction):
+    if direction == LEFT:
+        return RIGHT
+    elif direction == RIGHT:
+        return LEFT
+    elif direction == FORWARDS:
+        return BACKWARDS
+    elif direction == BACKWARDS:
+        return FORWARDS
+    else:
+        return None
 
-def create_wizard(map_dict):
-    pass
-    # TODO - pick a random room to place the wizard in
-    # TODO - pick a random health for the wizard
-    # TODO - create the monster with the health
+def check_connection(connections, room_name, direction, other_room_name):
+    for conect in connections:
+        r = conect.keys()[0]
+        d = conect[r].keys()[0]
+        o = conect[r].values()[0]
+        if r == room_name and d == opposite(direction):
+            return False
+        if o == other_room_name and d == direction:
+            return False
+    return True
+
+def create_monster(monster_num, room):
+    health = random.randint(10,25)
+    monster_name = "monster%d" % monster_num
+    monster = { 'class' : 'Monster', 'health': health }
+    room[monster_name] = monster
+
+def create_wizard(map_dict, num_rooms):
+    health = random.randint(20,30)
+    wizard  = { 'class' : 'wizard', 'health': health }
+
+    room_num = random.randint(1, num_rooms)
+    room_name = "room%d" % room_num
+    room = map_dict[room_name]
+    room["wizard"] = wizard
     # TODO - add the wizard to the chosen room
 
 
